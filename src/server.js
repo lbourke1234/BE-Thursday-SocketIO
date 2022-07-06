48e52edf8377153a1728574624b237495055e8b0
lbourke1234 <lbourke1234@gmail.com>
2022-07-06 13:15:19 +0100

    update files

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-import express from "express"
-import { createServer } from "http"
-import { Server } from "socket.io"
-import mongoose from "mongoose"
-import cors from "cors"
-import listEndpoints from "express-list-endpoints"
-import roomsRouter from "./api/rooms/index.js"
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import listEndpoints from 'express-list-endpoints'
+import roomsRouter from './api/rooms/index.js'
 
-import connectionHandler from "./socket/index.js"
+import connectionHandler from './socket/index.js'
 
 const server = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 5001
 
 const httpServer = createServer(server)
 
@@ -18,17 +18,17 @@ server.use(cors())
 server.use(express.json())
 
 // ****************************************** ENDPOINTS **********************************
-server.use("/rooms", roomsRouter)
+server.use('/rooms', roomsRouter)
 
 // *************************************** ERROR HANDLERS ********************************
 
 const io = new Server(httpServer)
 
-io.on("connection", connectionHandler)
+io.on('connection', connectionHandler)
 
 mongoose.connect(process.env.MONGO_URL)
 
-mongoose.connection.on("connected", () => {
+mongoose.connection.on('connected', () => {
   httpServer.listen(port, () => {
     console.table(listEndpoints(server))
     console.log(`Server running on port ${port}`)
