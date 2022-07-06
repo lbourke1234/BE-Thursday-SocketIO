9e65605710465599f30d0dcef390554c1f6b5d05
lbourke1234 <lbourke1234@gmail.com>
2022-07-06 15:27:21 +0100

    push

--- src/api/rooms/index.js
+++ src/api/rooms/index.js
@@ -1,11 +1,11 @@
-import express from "express"
-import createError from "http-errors"
-import RoomsModel from "../../models/rooms.js"
-import MessagesModel from "../../models/messages.js"
+import express from 'express'
+import createError from 'http-errors'
+import RoomsModel from '../../models/rooms.js'
+import MessagesModel from '../../models/messages.js'
 
 const roomsRouter = express.Router()
 
-roomsRouter.post("/", async (req, res, next) => {
+roomsRouter.post('/', async (req, res, next) => {
   try {
     const newRoom = new RoomsModel(req.body)
     const { _id } = await newRoom.save()
@@ -15,14 +15,15 @@ roomsRouter.post("/", async (req, res, next) => {
   }
 })
 
-roomsRouter.get("/:roomName/messages", async (req, res, next) => {
+roomsRouter.get('/:roomName/messages', async (req, res, next) => {
   try {
     const room = await RoomsModel.findOne({ name: req.params.roomName })
+    console.log('ROOM : ', room)
     if (room) {
       const messages = await MessagesModel.find({ room: room._id })
       res.send(messages)
     } else {
-      next(createError(404, `Room with id ${req.params.roomName} not found!`))
+      next(createError(404, `Room with name ${req.params.roomName} not found!`))
     }
   } catch (error) {
     next(error)
--- src/socket/index.js
+++ src/socket/index.js
@@ -1,44 +1,51 @@
-import { saveMessage } from "../utils/messages.js"
+import { saveMessage } from '../utils/messages.js'
+import RoomsModel from '../models/rooms.js'
 
 let onlineUsers = []
 
-const connectionHandler = socket => {
-  // We have established a connection with a client
-  socket.emit("welcome", { message: `Hello ${socket.id}!` })
+const connectionHandler = (socket) => {
+  socket.emit('welcome', { message: `Hello ${socket.id}!` })
 
-  // FE is emitting setUsername event --> BE should listen for that
+  socket.on('setUsername', (payload) => {
+    onlineUsers.push({
+      username: payload.username,
+      socketId: socket.id,
+      room: payload.room
+    })
+    console.log('ONLINE USERS: ', onlineUsers)
 
-  socket.on("setUsername", payload => {
-    // When a new client connects to the chat and sets a username, BE should keep track of that socketId & username
-    onlineUsers.push({ username: payload.username, socketId: socket.id, room: payload.room })
-    console.log("ONLINE USERS: ", onlineUsers)
-
-    // To join a specific room we can use socket.join
     socket.join(payload.room)
 
-    console.log("ROOMS ", socket.rooms)
+    console.log('ROOMS ', socket.rooms)
 
-    // FE is waiting for an event called loggedin, we gonna emit that and send the list of online users
-    socket.emit("loggedin", onlineUsers)
-    // Also the other connected users should receive the list of current online users
-    socket.broadcast.emit("newConnection", onlineUsers) // We want to emit this event to every connected socket but not the current one
+    socket.emit('loggedin', onlineUsers)
+    socket.broadcast.emit('newConnection', onlineUsers)
   })
 
-  socket.on("sendmessage", async ({ message, room }) => {
-    // we should broadcast that message to everybody but not to the sender of the message (otherwise he would see a duplicated message on the chat)
-    // socket.broadcast.emit("message", message)
-
-    // we would like to save the message in db
+  socket.on('sendMessage', async ({ message, room }) => {
+    console.log('MESSAGE: ', message)
     await saveMessage(message, room)
 
-    // we would like to emit to everybody who is in the room
-    socket.to(room).emit("message", message)
+    socket.to(room).emit('message', message)
   })
 
-  socket.on("disconnect", () => {
-    // event automatically emitted by the FE when user closes the browser/tab
-    onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id)
-    socket.broadcast.emit("newConnection", onlineUsers)
+  socket.on('disconnect', () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id)
+    socket.broadcast.emit('newConnection', onlineUsers)
+  })
+
+  socket.on('privateChat', ({ mainUser, clickedUser }) => {
+    console.log('hello')
+    console.log('MAIN USER: ', mainUser)
+    console.log('CLICKED USER: ', clickedUser)
+
+    const privateRoom = new RoomsModel({
+      name: mainUser.username + clickedUser.username,
+      members: [mainUser.username, clickedUser.username]
+    })
+    privateRoom.save()
+
+    socket.join(privateRoom.name)
   })
 }
 
